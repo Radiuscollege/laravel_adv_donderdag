7668aa569256500e993888f050e920f7d24169ea
Fedde <feddevangils@gmail.com>
2018-11-29 14:41:26 +0100

    einde les 2
    Huiswerk is alleen de lijst van todos tonen die horen bij de ingelogde gebruiker en je mag de detailpagina alleen bekijken van een todo item die bij de ingelogde gebruiker hoort

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
+    public function __construct() {
+
+        $this->middleware('auth');
+
+    }
     /**
      * Display a listing of the resource.
      *
@@ -14,6 +19,7 @@ class TodoController extends Controller
      */
     public function index()
     {
+        // niet alle to do items, maar alleen van ingelogde.
         return Todo::all();
     }
 
