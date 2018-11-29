60095fcd5ff7dd4783045253509f1209d2f1cf94
Fedde <feddevangils@gmail.com>
2018-11-29 13:11:29 +0100

    Luke naar tandarts;

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -14,7 +14,7 @@ class TodoController extends Controller
      */
     public function index()
     {
-        //
+        return Todo::all();
     }
 
     /**
@@ -46,7 +46,7 @@ class TodoController extends Controller
      */
     public function show(Todo $todo)
     {
-        //
+        return $todo;
     }
 
     /**
